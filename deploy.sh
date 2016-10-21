7f7a3d355f1fb68b4a4f53a77eeb69fbcdc71e17
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-21 03:07:49 +0200

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+[ "$TRAVIS" == true ] || echo "Skipping deployment" && exit 1
+
 n=`node -e 'console.log(require("./index").name)'`
 v=`node -e 'console.log(require("./index").version)'`
+
+[ "$TRAVIS_REPO_SLUG" == rsp/$name ] || "Skipping deployment in repo $TRAVIS_REPO_SLUG" && exit 0
+[ "$TRAVIS_BRANCH" == master ]] || "Skipping deployment on branch $TRAVIS_BRANCH" && exit 0
+
 u=https://registry.npmjs.org/$n/$v
 s=`curl -s -o /dev/null -w "%{http_code}" $u`
 if [ "$s" == 200 ]; then
